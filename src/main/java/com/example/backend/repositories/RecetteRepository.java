8512d37dd791cd54c8ab4d1d4c0be0688ab095e8
2289642 <2289642@collegmev.qc.ca>
2024-04-25 13:57:51 -0400

    Didn't move folder properly

--- src/main/java/com/example/backend/repositories/RecetteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repositories;
-
-import com.example.backend.model.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecetteRepository extends JpaRepository<Recipe, Integer> {
-}
--- src/main/java/com/example/backend/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repositories;
-
-import com.example.backend.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Integer> {
-    public boolean existsByEmail(String email);
-    public boolean existsByUsername(String username);
-}
