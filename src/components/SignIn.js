70ab8cea31c1c95c253a087179d0d60c6f35d47b
2060485 <2060485@collegemv.qc.ca>
2024-05-01 22:09:41 -0400

    ui sign in

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -27,36 +27,35 @@ function SignIn() {
             });
     }
 
-
     return (
-        <>
-            <form onSubmit={handleSubmit} className="container rounded-top rounded-bottom mt-5">
-                <p className='mt-2'>Sign In</p>
-
-                <div className="form-group">
-                    <label htmlFor="exampleInputEmail1">Email address or pwd</label>
-                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value={inputValueEmail} onChange={handleInputChangeEmail}/>
+        <div className="mt-4 container border-0">
+            <div className="card">
+                <div className='card-header'>
+                    <h3>Sign In:</h3>
                 </div>
-                <div className='emailError'></div>
-
-                <div className="form-group">
-                    <label htmlFor="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" value={inputValuePassword} onChange={handleInputChangePassword}/>
+                <div className='card-header bg-white'>
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-3">
+                            <label className="form-label">Email address or pwd</label>
+                            <input type="text" className="form-control" name="email" value={inputValueEmail} onChange={handleInputChangeEmail}/>
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Password</label>
+                            <input type="password" className="form-control" name="password" value={inputValuePassword} onChange={handleInputChangePassword}/>
+                        </div>
+                        <button type="submit" className="btn btn-primary mt-3">Submit</button>
+                    </form>
                 </div>
-
-                <div className='passwordError'></div>
-                <button type="submit" className="btn btn-primary mt-2">Submit</button>
-
-                <br/>
-                <div className="signup mt-2">
-                    <div>No account? <a className="signup" href="/signUp"> Sign up</a></div>
+                <div className='card-footer'>
+                    <div className="signup mt-2">
+                        <div>No account? <a className="signup" href="/signUp"> Sign up</a></div>
+                    </div>
+                    <div className="signup mt-2">
+                        <div>Forgot password? <a className="reset" href="/resetPassword">Reset password</a></div>
+                    </div>
                 </div>
-                <br/>
-                <div className="signup mt-2">
-                    <div>Forgot password? <a className="reset" href="/resetPassword">Reset password</a></div>
-                </div>
-            </form>
-        </>
+            </div>
+        </div>
     );
 }
 
