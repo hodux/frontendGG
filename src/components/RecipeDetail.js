dc4039e2a64922a1cd1790819bf59145429a9275
Mathieu Gouveia Sousa <2233635@collegemv.qc.ca>
2024-05-08 13:38:29 -0400

    add back arrow to recipedetail

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 import '../css/recipeDetail.css';
+import { useNavigate } from 'react-router-dom';
 
 function RecipeDetail() {
     const [recipe, setRecipe] = useState({});
@@ -18,25 +19,42 @@ function RecipeDetail() {
             });
     }, [recipe_ID]);
 
+    const navigate = useNavigate(); // Initialize navigate function
+
+    const handleGoBack = () => {
+        navigate(-1); // Go back to the previous page
+    }
+
     return (
         <div className="container border-0 mt-4 mb-4">
+            {/* Back Arrow */}
+            <div className="row mb-3">
+                <div className="col">
+                    <button className="btn btn-secondary" onClick={handleGoBack}>
+                        <i className="bi bi-arrow-left"></i> Back
+                    </button>
+                </div>
+            </div>
+
             <div className="row">
                 <div className="col-lg-6 col-md-12">
-                    <img className='image img-fluid' src={recipe.img} alt={recipe.recipe_name} />
+                    <img className='image img-fluid' src={recipe.img} alt={recipe.recipe_name}/>
                 </div>
                 <div className="col-lg-6 col-md-12">
                     <div className='ms-3 info'>
                         <h1>{recipe.recipe_name}</h1>
                         <p><i className="bi bi-clock"></i> &nbsp;{recipe.preparationTime}</p>
-                        <p>Vegan {recipe.isVegan ? <i className="bi bi-check-lg"></i> : <i className="bi bi-ban"></i>}</p>
-                        <p>Végétarien {recipe.isVegetarian ? <i className="bi bi-check-lg"></i> : <i className="bi bi-ban"></i>}</p>
+                        <p>Vegan {recipe.isVegan ? <i className="bi bi-check-lg"></i> :
+                            <i className="bi bi-ban"></i>}</p>
+                        <p>Végétarien {recipe.isVegetarian ? <i className="bi bi-check-lg"></i> :
+                            <i className="bi bi-ban"></i>}</p>
                         <p>Calories: {recipe.calories}</p>
                         <h2>Description </h2>
                         <p>{recipe.descriptions}</p>
                         <h2>Ingredients</h2>
                         <p>{recipe.ingredients}</p>
                         <h2>Instruction</h2>
-                        <p dangerouslySetInnerHTML={{ __html: recipe.instructions }}></p>
+                        <p dangerouslySetInnerHTML={{__html: recipe.instructions}}></p>
                     </div>
                 </div>
             </div>
